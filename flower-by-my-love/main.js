84c391b0e139dd6b65917f909cf126f3581bbc4d
Amithnal Herath <121004983+amiiboii@users.noreply.github.com>
2025-03-01 13:18:31 -0800

    Delete flower-by-my-love directory

--- flower-by-my-love/main.js
+++
@@ -1,9 +0,0 @@
-onload = () => {
-    document.body.classList.remove("container");
-    document.querySelector('.music').play();
-    
-    // Show the message box after a delay (e.g., 5 seconds)
-    setTimeout(() => {
-        document.getElementById('messageBox').style.display = 'block';
-    }, 5000); // Adjust the delay as needed
-};
